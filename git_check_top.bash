309c37d10746489040fdf93d3c92c0cba2bf4e89
Liang Tang @ MacBook <liang.tang@digipen.edu>
2018-09-28 23:22:33 +0800

    Update git_check_top.bash file: a loop is implemented.

--- git_check_top.bash
+++ git_check_top.bash
@@ -4,10 +4,18 @@ mkdir result
 git log --all > log.txt
 g++ -c -o test.o test4.cpp -std=c++11
 g++ -o test.exe test.o
-./test.exe 2018 Sep 26 2018 Sep 27
 
-mv ./name.txt    ./result/
-mv ./commit.cmd  ./result
-#call %~dp0\result\commit.cmd
+cd ..
+for i in sep250_18f_team_a sep250_18f_team_b sep250_18f_team_c sep250_18f_team_d sep250_18f_team_e sep250_18f_team_f sep250_18f_team_g sep250_18f_team_h
+do
+   echo "I am in directory: $i"
+   pwd
 
-source ./result/commit.cmd
+#    ./test.exe 2018 Sep 26 2018 Sep 27
+#    
+#    mv ./name.txt    ./result/
+#    mv ./commit.cmd  ./result
+#    
+#    source ./result/commit.cmd
+done
+
